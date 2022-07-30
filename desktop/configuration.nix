8fbc69e5e699ea01189b2d34270dab624eed970a
miachi <miachi>
2022-07-30 18:42:28 +0700

    add dnscrypt

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -21,15 +21,42 @@
   boot.kernelParams = [ "quiet" ];
   # Zram
   zramSwap.enable = true;	
-  networking.hostName = "Nijigasaki-Hs"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Enable networking
+  networking = {
+    hostName="Nijigasaki-Hs";
+    nameservers = [ "127.0.0.1" "::1" ];
+    # If using dhcpcd:
+    dhcpcd.extraConfig = "nohook resolv.conf";
+    # If using NetworkManager:
+    networkmanager.dns = "none";
+    networkmanager.enable=true;
+  };
+  # Kominfo workaround
+  # Make sure you don't have services.resolved.enable on.
+  services.dnscrypt-proxy2 = {
+    enable = true;
+    settings = {
+      ipv6_servers = true;
+      require_dnssec = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+      sources.public-resolvers = {
+        urls = [
+          "https://raw.githubusercontent.com/DNSCrypt/dnscrypt-resolvers/master/v3/public-resolvers.md"
+          "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
+        ];
+        cache_file = "/var/lib/dnscrypt-proxy2/public-resolvers.md";
+        minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
+      };
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+      # You can choose a specific set of servers from https://github.com/DNSCrypt/dnscrypt-resolvers/blob/master/v3/public-resolvers.md
+       server_names = [ "cloudflare" ];
+    };
+  };
+
+  systemd.services.dnscrypt-proxy2.serviceConfig = {
+    StateDirectory = "dnscrypt-proxy";
+  };
+
 
   # Set your time zone.
   time.timeZone = "Asia/Jakarta";
