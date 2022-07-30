6e0f5758db80119c0e2e11f0fa02498a6d50f4b7
miachi <miachi>
2022-07-30 12:37:10 +0700

    add lot of stuff

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -21,7 +21,12 @@
   boot.kernelParams = [ "quiet" ];
   # Zram
   zramSwap.enable = true;	
-  networking.hostName = "foo"; # Define your hostname.
+  networking.hostName = "Nijigasaki-Hs"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -29,7 +34,6 @@
   # Set your time zone.
   time.timeZone = "Asia/Jakarta";
   time.hardwareClockInLocalTime=true;
-
   # Select internationalisation properties.
   i18n.defaultLocale = "id_ID.UTF-8";
 
@@ -39,9 +43,9 @@
   # Enable KDE Plasma.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+   
 
-
- # Configure keymap in X11
+  # Configure keymap in X11
   services.xserver = {
     layout = "us";
     xkbVariant = "";
@@ -59,13 +63,21 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.foo = {
+  users.users.miataylor = {
     isNormalUser = true;
-    description = "foo";
+    description = "MiaTaylor";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       
@@ -85,7 +97,8 @@
    programs.steam = {
     enable = true;
   };
-
+  # Piper
+  services.ratbagd.enable = true;
 
   # Internal HDD
   fileSystems."/mnt/foo"={
@@ -106,24 +119,28 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #fonts
   noto-fonts-cjk-sans
+  noto-fonts-emoji
   # browser
   google-chrome
   google-chrome-beta
   firefox
-
+  discord
   # dev
-  vscode
-
+  vscode  
   # multimedia
-  vlc   
+  vlc
+  ffmpeg   
   # misc
   htop
+  piper
   kate
   neofetch
   kde-gtk-config
   wget
+  filelight
+  flameshot
+  ark unrar
   plasma-browser-integration
   power-profiles-daemon
   ];
@@ -144,13 +161,37 @@
   "/nix".options =["compress=zstd" "noatime"];
   };
 
-  # List services that you want to enable:
+  # Fuck Kominfo
 
+	
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+  
+
+  # List services that you want to enable:
   # use flatpak
   services.flatpak.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk pkgs.xdg-desktop-portal-gnome  ];
-
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
   
-  system.stateVersion = "22.05";
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+  
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
 
 }
